58f8497e47cc7bad3856f1548fc754d4880aa362
roman <atsapura@gmail.com>
2019-06-14 14:43:07 +0200

    logging refactoring

--- CardManagement.Infrastructure/Logging.fs
+++ CardManagement.Infrastructure/Logging.fs
@@ -27,24 +27,27 @@ module Logging =
             let errorMessage = errorMessage e
             Log.Error(errorMessage)
 
+    let private logResult funcName result =
+        match result with
+        | Ok ok -> sprintf "%s finished with result\n%A" funcName ok |> Log.Information
+        | Error e ->
+            match box e with
+            | :? DataRelatedError as er -> logDataError funcName er
+            | :? Error as er -> logError funcName er
+            | :? ValidationError as er -> logValidationError funcName er
+            | :? OperationNotAllowedError as er -> logOperationNotAllowed funcName er
+            | e -> sprintf "%A" e |> Log.Error
+
     let logify funcName func x =
         sprintf "start %s with arg\n%A" funcName x |> Log.Information
         let result = func x
-        sprintf "%s finished with result\n%A" funcName result |> Log.Information
+        logResult funcName result
         result
 
     let logifyAsync funcName funcAsync x =
         async {
             sprintf "start %s with arg\n%A" funcName x |> Log.Information
             let! result = funcAsync x
-            match result with
-            | Ok ok -> sprintf "%s finished with result\n%A" funcName ok |> Log.Information
-            | Error e ->
-                match box e with
-                | :? DataRelatedError as er -> logDataError funcName er
-                | :? Error as er -> logError funcName er
-                | :? ValidationError as er -> logValidationError funcName er
-                | :? OperationNotAllowedError as er -> logOperationNotAllowed funcName er
-                | e -> sprintf "%A" e |> Log.Error
+            logResult funcName result
             return result
         }
